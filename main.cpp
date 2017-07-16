97ce17ecd0231d7e96a1b58e29d25e05c9098b0b
Diganta Mukhopadhyay <diganta.ghs@gmail.com>
2017-07-16 18:48:58 +0530

    Adding support to load a single asset and add to the scene

--- main.cpp
+++ main.cpp
@@ -22,26 +22,17 @@ int main()
                     1.0, 0.0};
     unsigned int tris[] = {0, 2, 1, 0, 3, 2};
 
-    RetiMesh mesh(verts, texes, 4, tris, 6);
-    mesh.getTransform().translateTransform(0.0, 0.0, -3.0);
+    //RetiMesh mesh(verts, texes, 4, tris, 6);
+    //mesh.getTransform().translateTransform(0.0, 0.0, -3.0);
 
-    rendr.addMesh(&mesh);
+    RetiSceneObject obj("/mnt/data/3DModels/Jug/Milk Jug.3ds");
+    //obj.getTransform().translateTransform(0, 0, -3);
+
+    rendr.addSceneObject(&obj);
 
     rendr.startRenderer();
 
-    while(true)
-    {
-        std::this_thread::sleep_for(std::chrono::milliseconds(5));
-        if (rendr.getKeyboard().isKeyPressed(RetiKey::ESCAPE))  break;
-        if (rendr.getKeyboard().isKeyPressed(RetiKey::UP))
-            rendr.getCamera().getTransform().translateTransform(0, 0, 0.005);
-        if (rendr.getKeyboard().isKeyPressed(RetiKey::DOWN))
-            rendr.getCamera().getTransform().translateTransform(0, 0, -0.005);
-        if (rendr.getKeyboard().isKeyPressed(RetiKey::LEFT))
-            rendr.getCamera().getTransform().rotateTransform(0.005, 0, 1, 0);
-        if (rendr.getKeyboard().isKeyPressed(RetiKey::RIGHT))
-            rendr.getCamera().getTransform().rotateTransform(-0.005, 0, 1, 0);
-    }
+    std::this_thread::sleep_for(std::chrono::seconds(5));
 
     rendr.stopRenderer();
 }
