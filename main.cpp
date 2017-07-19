b02f11cdaeb1d4dab597789e8e82e661f639eeae
Diganta Mukhopadhyay <diganta.ghs@gmail.com>
2017-07-19 11:03:39 +0530

    Doom style controls

--- main.cpp
+++ main.cpp
@@ -8,11 +8,11 @@ using namespace std;
 
 int main()
 {
-    RetiRenderer::setReticleRootDirectory("/mnt/data/git-repos/ProjectReticle/Reticle");
+    RetiRenderer::setReticleRootDirectory("/mnt/data/git-repos/merge-resolve/ProjectReticle/Reticle");
     RetiRenderer::init();
     RetiRenderer rendr;
 
-    float verts[] = {1.0, 1.0, 0.0,
+    /*float verts[] = {1.0, 1.0, 0.0,
                      -1.0, 1.0, 0.0,
                      -1.0, -1.0, 0.0,
                      1.0, -1.0, 0.0};
@@ -23,13 +23,36 @@ int main()
     unsigned int tris[] = {0, 2, 1, 0, 3, 2};
 
     RetiMesh mesh(verts, texes, 4, tris, 6);
-    mesh.getTransform().translateTransform(0.0, 0.0, -3.0);
+    mesh.getTransform().translateTransform(0.0, 0.0, -3.0);*/
 
-    rendr.addMesh(&mesh);
+    RetiSceneObject obj("/home/digumx/Models/jug2.3DS");
+
+    rendr.addSceneObject(&obj);
+    obj.getTransform().translateTransform(0, 0, -10);
 
     rendr.startRenderer();
 
-    std::this_thread::sleep_for(std::chrono::seconds(20));
+    rendr.getKeyboard().addKeyListener(RetiKey::ENTER);
+    rendr.getKeyboard().addKeyListener(RetiKey::UP);
+    rendr.getKeyboard().addKeyListener(RetiKey::DOWN);
+    rendr.getKeyboard().addKeyListener(RetiKey::LEFT);
+    rendr.getKeyboard().addKeyListener(RetiKey::RIGHT);
+
+    while(true)
+    {
+        std::this_thread::sleep_for(std::chrono::milliseconds(5));
+        obj.getTransform().rotateTransform(0.009, 0, 1, 0);
+        if(rendr.getKeyboard().isKeyPressed(RetiKey::ENTER))
+            break;
+        if(rendr.getKeyboard().isKeyPressed(RetiKey::UP))
+            rendr.getCamera().getTransform().translateTransform(0, 0, 0.003);
+        if(rendr.getKeyboard().isKeyPressed(RetiKey::DOWN))
+            rendr.getCamera().getTransform().translateTransform(0, 0, -0.003);
+        if(rendr.getKeyboard().isKeyPressed(RetiKey::LEFT))
+            rendr.getCamera().getTransform().rotateTransform(-0.003, 0, 1, 0);
+        if(rendr.getKeyboard().isKeyPressed(RetiKey::RIGHT))
+            rendr.getCamera().getTransform().rotateTransform(0.003, 0, 1, 0);
+    }
 
     rendr.stopRenderer();
 }
