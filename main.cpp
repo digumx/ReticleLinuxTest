71009235505323f2b03b7fcf0f0fed25d0808b15
Diganta Mukhopadhyay <diganta.ghs@gmail.com>
2017-07-21 23:17:47 +0530

    General test case setup

--- main.cpp
+++ main.cpp
@@ -1,34 +1,69 @@
 #include <iostream>
 #include <thread>
+#include <cmath>
 #include <pthread.h>
 #include <chrono>
 #include <reticle.h>
+
+/// Uncomment to parent camera to obj.
+#define PARENT_CAM
+
+/** Note, for best results, activate only one of the following 3 macros at a time. */
+
+/** Uncomment to test global vs local rotations*/
+#define TEST_LOCAL_ROTATION
+
+/** Uncomment to test global vs local scale*/
+//#define TEST_LOCAL_SCALE
+
+/** Uncomment to test global vs local scale*/
+//#define TEST_LOCAL_TRANSLATION
+
 
 using namespace std;
 
 int main()
 {
-    RetiRenderer::setReticleRootDirectory("/mnt/data/git-repos/merge-resolve/ProjectReticle/Reticle");
+    RetiRenderer::setReticleRootDirectory("/mnt/data/Dev/git-reticle/ProjectReticle/Reticle");
     RetiRenderer::init();
     RetiRenderer rendr;
-
-    /*float verts[] = {1.0, 1.0, 0.0,
-                     -1.0, 1.0, 0.0,
-                     -1.0, -1.0, 0.0,
-                     1.0, -1.0, 0.0};
-    float texes[] = {1.0, 1.0,
-                    0.0, 1.0,
-                    0.0, 0.0,
-                    1.0, 0.0};
-    unsigned int tris[] = {0, 2, 1, 0, 3, 2};
-
-    RetiMesh mesh(verts, texes, 4, tris, 6);
-    mesh.getTransform().translateTransform(0.0, 0.0, -3.0);*/
 
     RetiSceneObject obj("/home/digumx/Models/jug2.3DS");
 
     rendr.addSceneObject(&obj);
-    obj.getTransform().translateTransform(0, 0, -10);
+    obj.getTransform().scaleTransform(20, 20, 20);
+    obj.getTransform().translateTransform(0, -2, -10);
+
+    RetiSceneObject obj2("/home/digumx/Models/Chair/Armchair.3DS");
+
+    rendr.addSceneObject(&obj2);
+    obj2.getTransform().translateTransform(0, -4, -30);
+    obj2.getTransform().scaleTransform(0.1, 0.1, 0.1);
+
+    #ifdef PARENT_CAM
+    rendr.getCamera().getTransform().setParent(obj.getTransform());
+    #endif // PARENT_CAM
+
+    #ifdef TEST_LOCAL_ROTATION
+    obj.getTransform().rotateTransform(1.57, 0, 0, 1);
+    #endif // TEST_LOCAL_ROTATION
+
+    #ifdef TEST_LOCAL_SCALE
+
+    /// Comment the following line to try different orientations.
+    obj.getTransform().rotateTransform(1.57, 0, 0, 1);
+    obj.getTransform().scaleTransformLocal(1, 10, 1);
+
+    #endif // TEST_LOCAL_SCALE
+
+    #ifdef TEST_LOCAL_TRANSLATION
+
+    /// Comment the following line to try different orientations.
+    obj.getTransform().rotateTransform(1.57, 0, 0, 1);
+    obj.getTransform().translateTransform(1, 10, 1);
+
+    #endif // TEST_LOCAL_TRANSLATION
+
 
     rendr.startRenderer();
 
@@ -37,21 +72,43 @@ int main()
     rendr.getKeyboard().addKeyListener(RetiKey::DOWN);
     rendr.getKeyboard().addKeyListener(RetiKey::LEFT);
     rendr.getKeyboard().addKeyListener(RetiKey::RIGHT);
+    rendr.getKeyboard().addKeyListener(RetiKey::W);
+    rendr.getKeyboard().addKeyListener(RetiKey::S);
 
     while(true)
     {
         std::this_thread::sleep_for(std::chrono::milliseconds(5));
-        obj.getTransform().rotateTransform(0.009, 0, 1, 0);
+
+        #ifdef TEST_LOCAL_ROTATION
+
+        /** Change rotateTransformLocal to rotateTransform to see global rotation */
+        obj.getTransform().rotateTransformLocal(0.003, 0, 1, 0);
+
+        #endif // TEST_LOCAL_ROTATION
+
+
         if(rendr.getKeyboard().isKeyPressed(RetiKey::ENTER))
             break;
         if(rendr.getKeyboard().isKeyPressed(RetiKey::UP))
-            rendr.getCamera().getTransform().translateTransform(0, 0, 0.003);
+        {
+            rendr.getCamera().getTransform().translateTransformLocal(0, 0, -0.009);
+
+            /** Uncomment the following, build, run and move in the forward direction. If indeed
+            *   camera transform is not inverted, the cup should not move in view space.
+            */
+            ///obj.getTransform().translateTransform(0, 0, -0.009);
+        }
         if(rendr.getKeyboard().isKeyPressed(RetiKey::DOWN))
-            rendr.getCamera().getTransform().translateTransform(0, 0, -0.003);
+            rendr.getCamera().getTransform().translateTransformLocal(0, 0, 0.009);
         if(rendr.getKeyboard().isKeyPressed(RetiKey::LEFT))
-            rendr.getCamera().getTransform().rotateTransform(-0.003, 0, 1, 0);
+            rendr.getCamera().getTransform().rotateTransformLocal(0.009, 0, 1, 0);
         if(rendr.getKeyboard().isKeyPressed(RetiKey::RIGHT))
-            rendr.getCamera().getTransform().rotateTransform(0.003, 0, 1, 0);
+            rendr.getCamera().getTransform().rotateTransformLocal(-0.009, 0, 1, 0);
+        if(rendr.getKeyboard().isKeyPressed(RetiKey::W))
+            rendr.getCamera().getTransform().rotateTransformLocal(0.009, 1, 0, 0);
+        if(rendr.getKeyboard().isKeyPressed(RetiKey::S))
+            rendr.getCamera().getTransform().rotateTransformLocal(-0.009, 1, 0, 0);
+
     }
 
     rendr.stopRenderer();
